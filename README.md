1590b331be5f04d370733d598a4814c5d2f472a4
Matheus Ferreira <matheusmlfg@gmail.com>
2023-12-06 23:00:26 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+TDD with Python
+https://www.obeythetestinggoat.com/pages/book.html
+
+CAPITULO 6
+Improving Functional Tests: Ensuring Isolation and Removing Voodoo Sleeps
---
+++ ft.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import unittest
+import time
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_can_start_a_todo_list(self):
+        
+        self.browser.get('http://localhost:8000')
+        self.assertIn("To-Do", self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do",header_text)
+        
+        
+        inputbox = self.browser.find_element(By.ID,"id_new_item")
+        inputbox.send_keys("Buy peacock feathers")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        inputbox = self.browser.find_element(By.ID,"id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        
+        table = self.browser.find_element(By.ID, "id_list_table")  
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(
+            "1: Buy peacock feathers", [row.text for row in rows]
+        )
+        self.assertIn(
+            "2: Use peacock feathers to make a fly",
+            [row.text for row in rows],
+        )
+        self.fail("Finish the Test!")
+        
+if __name__ == "__main__":
+    unittest.main()
